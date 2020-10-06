f5f446c0dc7d4fb52e39a652855557795d780b05
mitjapotocin <mitja.potocin@gmail.com>
2020-10-06 21:49:20 +0200

    Removed clg's

--- src/js/main.js
+++ src/js/main.js
@@ -203,7 +203,6 @@ function gameOver() {
 
 async function postScore() {
   if (resultsObject[nickname] === undefined || resultsObject[nickname] < scoreCount) {
-    console.log('post')
     await postData(apiURL, { name: nickname, score: scoreCount })
 
     getResults().then(() => {
@@ -278,18 +277,12 @@ async function postData(url = '', data = {}) {
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
     mode: 'cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    // credentials: '', // include, *same-origin, omit
+
     headers: {
       'Content-Type': 'application/json',
-      // 'Content-Type': 'application/x-www-form-urlencoded',
     },
     redirect: 'follow', // manual, *follow, error
     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(data), // body data type must match "Content-Type" header
   })
-  console.log(response.status)
 }
-
-// postData(apiURL, { name: 'Simon', score: 300 }).then((data) => {
-//   console.log(data) // JSON data parsed by `data.json()` call
-// })
